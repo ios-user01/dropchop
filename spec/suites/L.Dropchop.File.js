a0ec867b512ff6825a3ded2d09ebc8d805cca470
Anthony Lukach <anthonylukach@gmail.com>
2015-06-17 22:32:34 -0600

    Begin buildout of file operations tests

--- spec/suites/L.Dropchop.File.js
+++ spec/suites/L.Dropchop.File.js
@@ -1,15 +1,31 @@
 describe("L.Dropchop.FileExecute Operations", function(){
-	var menu;
-	var ops;
-	var exampleData = true;
+    var menu;
+    var ops;
+    var exampleData = true;
 
-	// TODO: test for geojson save
-	describe('Save as a geojson', function () {
-		expect(exampleData).to.eql(true);
-	});
+    // TODO: test for geojson save
+    describe('Save as a geojson', function () {
+        expect(exampleData).to.eql(true);
+    });
 
-	// TODO: test for shapefile save
-	describe('Save as a shapefile', function () {
-		expect(exampleData).to.eql(true);
-	});
+    // TODO: test for shapefile save
+    describe('Save as a shapefile', function () {
+        expect(exampleData).to.eql(true);
+    });
+
+    describe('load from url', function () {
+
+    });
+
+    describe('load from gist', function () {
+
+    });
+
+    describe('getRequest', function () {
+
+    });
+
+    describe('_addJsonAsLayer', function () {
+        // TODO: This should be tested heavily
+    });
 });
